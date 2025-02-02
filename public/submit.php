6c7e2a951d22844d6269dbc421cea75daad66bcd
Moamen-Wady <moamen.wady@gmail.com>
2025-02-02 10:23:46 +0200

    Final

--- public/submit.php
+++ public/submit.php
@@ -2,7 +2,9 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
-$message = $_POST['message'];
+$activity = $_POST['activity'];
+$residence = $_POST['residence'];
+$notes = $_POST['notes'];
 
 $to = " moamen.wady@gmail.com";
 $subject = "New Daman profile form submission: $name";
@@ -10,7 +12,9 @@ $subject = "New Daman profile form submission: $name";
 $body = "Name: $name\n";
 $body .= "Email: $email\n";
 $body .= "Phone: $phone\n";
-$body .= "Message: $message\n";
+$body .= "Activity: $activity\n";
+$body .= "Residence: $residence\n";
+$body .= "Notes: $notes\n";
 
 $headers = "From: $email\r\n";
 $headers .= "Reply-To: $email\r\n";
